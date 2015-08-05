051280e489b5cc79c42d15a9ac43d71ad53e0be5
upshan <369361116@qq.com>
2015-08-05 09:40:05 +0800

    获取 tree item Name

--- app/controllers/wap/FileUploadController.java
+++ app/controllers/wap/FileUploadController.java
@@ -24,25 +24,18 @@ public class FileUploadController extends Controller {
     }
 
     public static void folderJSON() {
-        //查询 一级类别
         List<FolderPropertie> topFolderList = FolderPropertie.findByTopFolder();
-        // 定义一个顶级 FolderJSON  List
         List<FolderJSON> topFolderJsonList = new ArrayList<>();
-        // 定义一个 topFolderJSON 循环使用
         FolderJSON topFolderJSON = null;
         for(FolderPropertie topFolder : topFolderList) {
             topFolderJSON = new FolderJSON();
             Long count = FolderPropertie.countByParentFolder(topFolder.id);
             topFolderJSON.title = topFolder.name;
             topFolderJSON.type = count > 0 ? "folder" : "item";
-            // 如果有子菜单
             if(count > 0) {
-                // 查询子菜单
                 List<FolderPropertie> childFolderList = FolderPropertie.findByParentFolder(topFolder.id);
-                // 定义 子菜单的 FolderJSON List
                 List<FolderJSON> folderJSONList = new ArrayList<>();
                 FolderJSON folderJSON = null;
-                // 循环子菜单
                 for(FolderPropertie childFolder : childFolderList) {
                     folderJSON = new FolderJSON();
                     folderJSON.title = childFolder.name;
