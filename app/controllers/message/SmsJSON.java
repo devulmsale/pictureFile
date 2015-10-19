97c7f49ebe3aedbf9aa364fe5b454d64b122a4ba
upshan <369361116@qq.com>
2015-10-19 10:36:19 +0800

    修改一下接口

--- app/controllers/message/SmsJSON.java
+++ app/controllers/message/SmsJSON.java
@@ -1,7 +1,5 @@
 package controllers.message;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import me.chanjar.weixin.common.util.StringUtils;
 import models.mer.Merchant;
 import play.Logger;
@@ -9,7 +7,6 @@ import play.mvc.Controller;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
